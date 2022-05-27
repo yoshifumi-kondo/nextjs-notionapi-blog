af7eafbb7d7540a49cfff8cfdf65ae8cf9584b4e
yoshifumi-kondo <kondoy@moonbase.tokyo>
2022-05-27 15:10:08 +0900

    set meta tags for google console

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,11 @@
 import { AppProps } from 'next/app';
 import '@/styles/globals.css';
+import MetaTags from '@/components/templates/MetaTgas';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <MetaTags />
       <Component {...pageProps} />
     </>
   );
